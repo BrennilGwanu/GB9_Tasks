3e331e151f5f72d0069d68c7577f7aa4f351a5b4
Snake <per.mortis.non.lates@gmail.com>
2023-03-02 22:25:38 +0500

    comleted

---
+++ Task64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string PrintNumbers(int n)
+{
+    if (n == 1)
+        return "1";
+    else
+    {
+        return  $"{n} " + PrintNumbers(n - 1);
+    }
+}
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Введите число N: ");
+    int number = int.Parse(Console.ReadLine()!);
+    Console.WriteLine(PrintNumbers(number));
+}
+
+Main();
---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int m, int n)
+{
+    if (m == n)
+        return m;
+    else
+    {
+        return SumNumbers(m, n - 1) + n;
+    }
+}
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Введите число M: ");
+    int m = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите число N: ");
+    int n = int.Parse(Console.ReadLine()!);
+    Console.WriteLine(SumNumbers(m, n));
+}
+
+Main();
